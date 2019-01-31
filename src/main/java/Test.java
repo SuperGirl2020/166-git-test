88e4d13544dac053d75868ddabde81e5d60e89a6
zbl <1060308799@qq.com>
2019-01-31 17:27:50 +0800

    yuxi提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,7 @@
  * Created by Administrator on 2019/1/31 0031.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    }
 }
